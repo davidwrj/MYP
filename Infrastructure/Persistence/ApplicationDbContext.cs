eb874ef359e4486377bc2df5d33a0c2fc674c77a
David Wang <davidavid76@hotmail.com>
2023-02-24 05:55:53 +1000

    VS2022 issue, commit to prevent lose of work

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Fluent.Infrastructure.FluentModel;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,25 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Persistence
 {
-    internal class ApplicationDbContext
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>, IApplicationDbContext
     {
+        private readonly ICurrentUserService _currentUserService;
+        private readonly IDateTime _dateTime;
+        private readonly IDomainEventService _domainEventService;
+
+        public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions,
+        ICurrentUserService currentUserService,
+        IDomainEventService domainEventService,
+        IDateTime dateTime) : base(options, operationalStoreOptions)
+        {
+            _currentUserService = currentUserService;
+            _domainEventService = domainEventService;
+            _dateTime = dateTime;
+        }
+
+        public DbSet<TodoList> TodoLists => Set<TodoList>();
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
     }
 }
